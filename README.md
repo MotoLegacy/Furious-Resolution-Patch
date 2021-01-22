844cf2f160cbd6c8bdf5029dee066a560d1ba3bc
unknown <motolegacy.git@gmail.com>
2021-01-22 13:53:57 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Furious-Resolution-Patch
-A BepInEx Plugin Designed to add Resolution and Fullscreen options to Garfield Kart: Furious Racing (https://store.steampowered.com/app/1085510/Garfield_Kart__Furious_Racing/)
+
+![](images/image.png)
+
+<hr>
+
+## About
+The Furious-Resolution-Patch is a BepInEx Plugin Designed to add Resolution and Fullscreen options to Garfield Kart: Furious Racing (https://store.steampowered.com/app/1085510/Garfield_Kart__Furious_Racing/). It is very generic, using only the basic Unity GUI Elements and API, so it will probably work on other games if applicable.
+
+<hr>
+
+## Usage
+After plugin initialization, the mouse cursor will become visible and a new button, `Show Screen Options`, will appear. Click the button to Display the Fullscreen Toggle and Window dimension settings. Adjust it to your liking and press `Apply Settings` for changes to take effect.
+
+<hr>
+
+## Installation
+The plugin requires [BepInEx](https://github.com/BepInEx/BepInEx) to be installed in the game's root directory, instructions to do so can be found on the BepInEx README. Once Installed, place `garfield_res.dll` into `<Game Root>/BepInEx/plugins/`. Once the game has launched, the plugin will initialize.
+
+<hr>
+
+## Building from Source
+Building requires the [.NET SDK](https://dotnet.microsoft.com/download) (5.0 or Newer) and can only be built on Windows systems. The following DLLS must be placed in the `lib` directory:
+
+From BepInEx:
+<ul>
+    <li>0Harmony.dll</li>
+    <li>BepInEx.dll</li>
+    <li>BepInEx.Harmony.dll</li>
+</ul>
+
+From Garfield Kart: Furious Racing:
+<ul>
+    <li>Assembly-CSharp.dll</li>
+    <li>UnityEngine.CoreModule.dll</li>
+    <li>UnityEngine.dll</li>
+    <li>UnityEngine.IMGUIModule.dll</li>
+    <li>UnityEngine.UI.dll</li>
+</ul>
+
+Once placed, simply run `dotnet build` in the root source directory to have the .DLL built to /bin/Debug/net48/.
---
+++ garfield_res.cs
@@ -0,0 +1,88 @@
+﻿//
+// MIT License
+//
+// Copyright (c) 2020 MotoLegacy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using BepInEx;
+using HarmonyLib;
+using BepInEx.Harmony;
+
+namespace garfield_res
+{
+    [BepInPlugin("org.bepinex.plugins.garfieldres", "Furious Racing Resolution Settings", "1.0.0.0")]
+    [BepInProcess("Garfield Kart Furious Racing.exe")]
+    public class garfield_res : BaseUnityPlugin
+    {
+        bool ShowMenu;
+        bool Fullscreen_Enabled;
+        string FSString = "";
+        string Width = "";
+        string Height = "";
+
+        void Awake()
+        {
+            Console.WriteLine("[Resolution Patch] Initialized.");
+            Cursor.visible = true;
+
+            Width = string.Format("{0}", Screen.width);
+            Height = string.Format("{0}", Screen.height);
+
+            //Harmony harmony = new Harmony("com.motolegacy.garfieldres");
+            //harmony.PatchAll();
+        }
+
+        void OnGUI()
+        {
+            if (!ShowMenu) {
+                if (GUI.Button(new Rect(10, 10, 150, 50), "Show Screen Options")) {
+                    ShowMenu = true;
+                } 
+            } else {
+                if (GUI.Button(new Rect(10, 10, 150, 50), "Hide Screen Options")) {
+                    ShowMenu = false;
+                }
+
+                if (Fullscreen_Enabled)
+                    FSString = "Fullscreen: Enabled";
+                else
+                    FSString = "Fullscreen: Disabled";
+
+                if (GUI.Button(new Rect(10, 75, 150, 50), FSString)) {
+                    Fullscreen_Enabled = !Fullscreen_Enabled;
+                }
+
+                GUI.Box(new Rect(175, 10, 150, 50), "Window Width");
+                GUI.Box(new Rect(350, 10, 150, 50), "Window Height");
+                Width = GUI.TextField (new Rect (175, 35, 150, 25), Width, 40);
+                Height = GUI.TextField (new Rect (350, 35, 150, 25), Height, 40);
+
+                if (GUI.Button(new Rect(525, 10, 150, 50), "Apply Settings")) {
+                    Screen.SetResolution(int.Parse(Width), int.Parse(Height), false);
+                    Screen.fullScreen = Fullscreen_Enabled;
+                }
+            }
+        }
+    }
+}
